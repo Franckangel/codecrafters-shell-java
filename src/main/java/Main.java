9d4db9554863a8361f5081bcfaf98a805904737a
Franck Djatcha <djatchafranck@gmail.com>
2025-11-25 13:35:39 +0100

    Extend type command 02

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,11 @@
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        ArrayList<String> commandList = new ArrayList<>(Arrays.asList("exit", "echo", "type"));
+        ArrayList<String> buildInCommands = new ArrayList<>(Arrays.asList("exit", "echo", "type"));
         //Implement REPL 
         while(true){
         System.out.print("$ ");
@@ -17,10 +18,34 @@ public class Main {
             System.out.println(command.substring(5));
         }
         else if(command.startsWith("type ")){
-            if(commandList.contains(command.substring(5)))
-                System.out.println(command.substring(5) + " is a shell builtin");
-            else
-                System.out.println(command.substring(5) + ": not found");
+            String argument = command.substring(5);
+            if(buildInCommands.contains(argument))
+                System.out.println(argument + " is a shell builtin");
+            else{
+                //Get the PATH environment variable
+                String pathVariable = System.getenv("PATH");
+
+                //Parse the system path
+                String[] pathDirs = pathVariable.split(File.pathSeparator);
+
+                //Search through each directory
+                boolean commandFound = false;
+                File file = null;
+                for(String dir : pathDirs){
+                    file = new File(dir, argument);
+                    if(file.exists() && file.isFile() && file.canExecute()){
+                        commandFound = true;
+                        break;
+                    }
+                }
+
+                if(commandFound)
+                    System.out.println(argument + " is " + file.getAbsolutePath());
+                else
+                //If no file found
+                    System.out.println(argument + ": not found");
+            }
+                
         }
         else
             // Prints the "<command>: command not found" message
