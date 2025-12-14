e68bd05ed8e015c2368d42788acd57adff373539
Franck Djatcha <djatchafranck@gmail.com>
2025-12-14 05:51:30 +0100

    cd built in extended for tilde(~) handling 02

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -35,7 +35,7 @@ public class Main {
         String directory = command.substring(2).trim();
 
         if(directory.equals("~")){
-          currentDirectory = new File(System.getProperty("user.home"));
+          currentDirectory = new File(System.getProperty("user.dir"));
           continue;
         }
 
